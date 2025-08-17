6cca4e288fe74b5305d1c91c41302b001eb74ab4
ahargunyllib <billy.bpm03@gmail.com>
2025-08-17 23:45:24 +0700

    feat: refactor components and utilities to shared directory structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import { Toaster } from "@/shared/components/ui/sonner";
+import "@/shared/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Toaster } from "../components/ui/sonner";
-import "./globals.css";
 
 const inter = Inter({
   variable: "--font-inter",
